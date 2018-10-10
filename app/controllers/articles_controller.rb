c2d8066e3fcb43e448751ebae74038d835302e16
紀　祐司 <signtheswish@gmail.com>
2018-10-10 22:15:30 +0900

    remove comment out

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
+  
   def index
     @articles = Article.all
   end
